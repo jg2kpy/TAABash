8c1ca0902734be8299f0f9a290d84851321b93cd
jg2kpy <jlgutierrez2000@hotmail.com>
2021-10-13 19:35:23 -0300

    xd

--- SegundoParcialito/testdaemon.sh
+++ SegundoParcialito/testdaemon.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 services=`systemctl list-units --type=service --state=running`
+#ps -ax
 
-if echo $services | grep $1 > /dev/null
+if echo $services | grep -v grep | grep $1 > /dev/null
 then
     echo "El daemon $1 esta en ejecucion"
 else
