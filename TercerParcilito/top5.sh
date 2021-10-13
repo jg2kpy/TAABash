155e0c5fbb03b3058ff274b8c6dec75475a0b139
jg2kpy <jlgutierrez2000@hotmail.com>
2021-10-13 19:24:02 -0300

    commit

--- TercerParcilito/top5.sh
+++ TercerParcilito/top5.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-if [ ! $# -eq 1]; then
+if [ ! $# -eq 1 ]; then
     echo "Usage: $0 directory"
     exit -1
 fi
 
 if [[ -d $1 ]];then
     echo "Top 5 ficheros mas pesados"
-    ls -l $1 | sort -n -k 5 | tail -n 5 | awk '{print $9,"\t",$5}'
+    ls -lS $1 | grep -v "^total" | head -n 5 | awk '{print $9," ",$5}'
+    echo ""
     echo "Top 5 ficheros modificados correctamente"
-    find $1 -type f -printf "%T@ %p\n" |sort -nr| awk '{print $2}' | awk 'BEGIN{FS="/"} {print $9}' | head -5
+    ls -lt $1 | grep -v "^total" | head -n 5 | awk '{print $9}'
 else
     echo "No es un directorio"
 fi
