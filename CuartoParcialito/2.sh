88bfbf28d27e17356117c0c45a4b0dc74fafd8a8
jg2kpy <jlgutierrez2000@hotmail.com>
2021-10-19 12:27:15 -0300

    CuartoParcialito

---
+++ CuartoParcialito/2.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+estaSeguro(){
+    echo "Esta accion hara que el proceso termine y el dispositivo sea inutilizable"
+    echo "Esta seguro que desea terminar? [Y/n]"
+    read respuesta
+    if [[ $respuesta == "Y" ]];
+    then
+        echo "Script terminado abruptamente"
+        exit -1
+    fi
+}
+
+echo "Creando imagen de arranque..."
+
+trap estaSeguro SIGINT SIGTERM SIGSTOP
+
+if [ $# -eq 2 ]
+then
+    iso=$1
+    medio=$2
+else
+    echo "Introduza path del iso: "
+    read iso
+    
+    echo "Introduza path del dispositivo: "
+    read medio
+fi
+
+if [[ ! -f $iso ]]
+then
+    echo "No es un fichero"
+    #exit -1
+fi
+
+if [[ ! -f $medio ]]
+then
+    echo "El dispositivo no existe o no esta montando"
+    #exit -1
+fi
+
+tree /
+#sudo dd if=$iso of=$medio bs=1M status=progress
+echo "Ejecutando proceso..."
+
+echo "Scrip terminado exitosamente"
---
+++ CuartoParcialito/service.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+start(){
+    retorno=`sudo su -c "nohup $1 >> /var/log/$1.log &"`
+    echo $retorno >> /proc/run/$1.pid
+}
+
+stop(){
+    pkill $1
+}
+
+restart(){
+    stop $1
+    start $1
+}
+
+if [ $# -ne 2 ];
+then
+    echo "Usage: $0 <nombre_demonio> <accion>"
+    exit -1
+fi
+
+if [ $2 == "start" ];then
+    start $1
+elif [ $2 == "stop" ];then
+    stop $1
+elif [ $2 == "restart" ];then
+    restart $1
+else
+    echo "Esta accion no existe"
+fi
